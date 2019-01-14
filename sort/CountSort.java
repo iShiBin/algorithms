da91c4819421b0c9c36d25716cf84eceb4270b1a
SHI Bin <shibin@huobi.com>
2019-01-13 21:30:20 -0800

    sorting

---
+++ sort/CountSort.java
@@ -0,0 +1,11 @@
+package sort;
+//todo
+//works on very spark less numbers, big range
+public class CountSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
